1a2bd587411ca8d3f1d1444fdb9eabbe9ccc4068
Alexandre Dória <alexandredoria.01@hotmail.com>
2014-01-30 10:17:58 -0300

    Primeiros códigos PHP
    Feita tela de login e incorporado código PHP

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Formulários - Restart</title>
+
+    <!-- Núcleo CSS do Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  </head>
+
+  <body>
+    
+      <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+          <div class="panel panel-default">
+            <!-- Default panel contents -->
+             
+            
+            
+            <div class="panel-body">
+              <center><p><img src="logo.png" class="img-responsive" alt="Restart"></p></center><br>
+              <form role="form" method="post" action="check.php">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Nome de usuário">
+                </div>           
+                <div class="form-group">
+                  <input type="password" class="form-control" placeholder="Senha">
+                </div>
+              </form>
+              <button type="submit" class="btn btn-default">Entrar</button>
+            </div>
+             
+         
+        </div><!-- /.row -->
+      <div class="col-lg-4"></div>
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+  </body>
+</html>
