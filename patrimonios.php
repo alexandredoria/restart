9d91ed6a9f208bcf007b50fbadf59e2a5340aaab
Alexandre Dória <alexandredoria.01@hotmail.com>
2014-02-23 13:34:02 -0300

    Exclusão de usuários e patrimônios bem-sucedida

--- patrimonios.php
+++ patrimonios.php
@@ -9,10 +9,11 @@ session_start();
     exit;
 
   }
-  $pageTitle  = "Usuários &middot; Visão Geral";
+  $pageTitle  = "Patrimônios &middot; Visão Geral";
   
   include 'nucleo/cabecario.php';
-  include 'classes/usuario.class.php';
+  
+  include 'classes/patrimonio.class.php';
   
 ?>
 <script language="JavaScript">
@@ -36,8 +37,10 @@ function toggle(source) {
 
 }
 
-
-
+$(document).on("click", ".abre-excluirModal", function () {
+     var IdPat = $(this).data('id');
+     $(".modal-body #IdPatrimonio").val( IdPat );
+});
 
 
 </script>
@@ -48,12 +51,12 @@ function toggle(source) {
         include("nucleo/barraLateral.php");
 
         if (!empty($_POST)) {
-          if (isset($_POST['id_action'])) {
-            $del_matricula   = $_POST['id_action'];
+          if (isset($_POST['IdPatrimonio'])) {
+            $num_patrimonio   = $_POST['IdPatrimonio'];
            
-            $delUser  = new Usuario;
-            $delUser->deletarUsuario($del_matricula);
-            unset($delUser);
+            $objPatrimonio  = new Patrimonio;
+            $objPatrimonio->deletarPatrimonio($num_patrimonio);
+            unset($objPatrimonio);
           }
         }
       ?>
@@ -62,17 +65,17 @@ function toggle(source) {
 
        <div class="row">
            <div class="col-lg-6">
-              <h1>Usuários <small>Visão geral</small></h1>
+              <h1>Patrimônios <small>Visão geral</small></h1>
             </div>
             <div class="col-lg-6" align="right">
-              <a href="cadUsuario.php"><button type="button" class="btn btn-primary"><i class="glyphicon glyphicon-plus-sign"></i> Novo usuário</button></a>
+              <a href="cadPatrimonio.php"><button type="button" class="btn btn-primary"><i class="glyphicon glyphicon-plus-sign"></i> Novo patrimônio</button></a>
             </div>      
        </div><!-- /.row -->
 
         <div class="row">
           <div class="col-lg-12">            
             <ol class="breadcrumb">
-               <li class="active"><i class="glyphicon glyphicon-user"></i> Usuários</li>
+               <li class="active"><i class="glyphicon glyphicon-user"></i> Patrimônios</li>
             </ol>
          
           </div>
@@ -84,9 +87,7 @@ function toggle(source) {
 
             <table>
               <tr>
-                  <th></th>
-                  <th></th>
-                  <th></th>
+                  <th colspan="2"></th>
                   
               </tr>
               <tr>
@@ -114,14 +115,17 @@ function toggle(source) {
                 <p>
                 <tr>
                   <th></th>
+                  
                   <th colspan="3">Operações</th>
-                  <th>Matrícula</th>
-                  <th>Nome</th>
-                  <th>Tipo de usuário</th>
-                  <th>Email</th>
-                  <th>Tel. Residencial</th>
-                  <th>Tel. Celular</th>
+                  <th>Registro</th>
+                  <th>Tipo</th>
+                  <th>Posição</th>
+                  <th>Situação</th>
+                  <th>Configuração</th>
+                  <th>Laboratório</th>
+                  <th>Data de cadastro</th>
                   <th>Data de atualização</th>
+                  
                   
 
                 </tr>
@@ -130,43 +134,51 @@ function toggle(source) {
                   <?php
                     
 
-                    $listaUser    = new Usuario;
-                    $result     = $listaUser->listarUsuarios($_SESSION['matricula']);
+                    $listaPatrimonio    = new Patrimonio;
+                    $result     = $listaPatrimonio->listarPatrimonios();
                     if (is_array($result)) {
                       foreach ($result as $row) {
-                        if ($row['tipo_usuario'] == 2){
-                          $tipo = "Bolsista";
-                        } if ( $row['tipo_usuario'] == 3){
-                          $tipo = "Professor";
+                        if ($row['tipo'] == 1){
+                          $tipo = "Monitor";
+                        } if ( $row['tipo'] == 2){
+                          $tipo = "Gabinete";
                         }
-                        if($row['data_atualizacao']===null){echo "<tr id='fooTr'class='danger'>";} else echo "<tr id='fooTr'>";
-                        echo "
-                            <td ><input type='checkbox'   name='foo' value='".$row['matricula']."'></td>
 
+                        if ($row['situacao'] == '1'){
+                          $situacao = "Ativo";
+                        } if ( $row['situacao'] == '2'){
+                          $situacao = "Desativado";
+                        }
+                        if($row['situacao'] == 2){echo "<tr id='fooTr'class='danger'>";} else echo "<tr id='fooTr'>";
+                        echo "
+                            <td ><input type='checkbox'   name='foo' value='".$row['num_patrimonio']."'></td>
                             <td>
-                              <a title='Ver usuário' data-toggle='modal' data-id='".$row['matricula']."' href='#modal_verPatrimonio' class='abre-verModal'>                              
+                              <a title='Ver patrimônio' data-toggle='modal' data-id='".$row['num_patrimonio']."' href='#modal_verPatrimonio' class='abre-verModal'>                              
                                  <i class='glyphicon glyphicon-search'></i>
                                </a>
                             </td>
 
                             <td>
-                              <a title='Editar usuário' data-toggle='modal' data-id='".$row['matricula']."' href='#modal_editPatrimonio' class='abre-editarModal'>                              
+                              <a title='Editar patrimônio' data-toggle='modal' data-id='".$row['num_patrimonio']."' href='#modal_editPatrimonio' class='abre-editarModal'>                              
                                  <i class='glyphicon glyphicon-pencil'></i>
                                </a>
                             </td>
 
                             <td>
-                              <a title='Excluir usuário' data-toggle='modal' data-id='".$row['matricula']."' href='#modal_excPatrimonio' class='abre-excluirModal'>                              
+                              <a title='Excluir patrimônio' data-toggle='modal' data-id='".$row['num_patrimonio']."' href='#modal_excPatrimonio' class='abre-excluirModal'>                              
                                  <i class='glyphicon glyphicon-remove'></i>
                                </a>
                             </td>
-                            <td>" . $row['matricula'] . "</td>
-                            <td>". $row['nome'] . " ".$row['sobrenome']."</td>
+
+                            
+                            <td>" . $row['num_patrimonio'] . "</td>
                             <td>" . $tipo . "</td>
-                            <td>" . $row['email'] . "</td>
-                            <td>" . $row['telefone_residencial'] . "</td>
-                            <td>" . $row['telefone_celular'] . "</td>
-                            <td>"; if ($row['data_atualizacao']===null){echo "Pendente";} else {echo date('d/m/Y', strtotime($row['data_atualizacao']));} echo "</td>
+                            <td>" . $row['num_posicionamento'] . "</td>
+                            <td>" . $situacao . "</td>
+                            <td>" . $row['Configuracao_id'] . "</td>
+                            <td>" . $row['Laboratorio_id'] . "</td>
+                            <td>"; if ($row['data_cadastro']===null){echo "";} else {echo date('d/m/Y', strtotime($row['data_cadastro']));} echo "</td>
+                            <td>"; if ($row['data_atualizacao']===null){echo date('d/m/Y', strtotime($row['data_cadastro']));} else {echo date('d/m/Y', strtotime($row['data_atualizacao']));} echo "</td>
                             
                             
                           </tr>"; 
@@ -194,18 +206,21 @@ function toggle(source) {
         </div><!-- /.row -->
 
         <!-- Button trigger modal -->
-        <div class='modal fade' id='modal_excUsuario' tabindex='-1' role='dialog' aria-labelledby='modal_excUsuarioLabel' aria-hidden='true'>
+        <div class='modal fade' id='modal_excPatrimonio' tabindex='-1' role='dialog' aria-labelledby='modal_excPatrimonioLabel' aria-hidden='true'>
   <div class='modal-dialog'>
     <div class='modal-content panel-danger'>
       <div class='modal-header panel-heading'>
         <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-        <h4 class='modal-title' id='modal_cadUsuarioLabel'>O usuário será excluído</h4>
+        <h4 class='modal-title' id='modal_cadPatrimonioLabel'>O patrimônio será excluído</h4>
       </div>
+      <form role="form" id="confirm" action="patrimonios.php" method="post">
       <div class='modal-body'>
+        
         Você realmente deseja executar essa operação?
+        <input type="hidden" name="IdPatrimonio" id="IdPatrimonio" value=""/>
       </div>
        <div class="modal-footer">
-        <form role="form" id="confirm" action="usuarios.php" method="post">
+        
             
           <button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
           <button type="submit" class="btn btn-danger">Sim</button>
